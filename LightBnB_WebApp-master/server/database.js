6fde92bd9c47b0c9bbf98cd78ef381e3e661aaf4
doublefriedtofu <hurdonghyuk@gmail.com>
2022-11-17 16:34:00 -0500

    refactored getAllReservations to show all reservations for signed-in guests using query

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -55,7 +55,11 @@ exports.getUserWithId = getUserWithId;
  */
 const addUser = function(user) {
   return pool
-    .query(`INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING *;`,
+    .query(
+      `INSERT INTO users (name, email, password) 
+      VALUES ($1, $2, $3) 
+      RETURNING *
+      ;`,
     [user.name, user.email, user.password])
     .then(result => {
       return result.rows[0];
@@ -75,7 +79,24 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  return pool
+    .query(
+    `SELECT reservations.*, properties.*, avg(rating) as average_rating
+    FROM reservations
+    JOIN properties ON reservations.property_id = properties.id
+    JOIN property_reviews ON properties.id = property_reviews.property_id
+    WHERE reservations.guest_id = $1
+    GROUP BY properties.id, reservations.id
+    ORDER BY reservations.start_date
+    LIMIT $2    
+    ;`, [guest_id, limit])
+    .then((result) => {
+      return result.rows;
+    })
+    .catch((err) => {
+      console.log("Error function getAllReservations: ", err.message);
+      throw(err.message);
+    })
 };
 exports.getAllReservations = getAllReservations;
 
